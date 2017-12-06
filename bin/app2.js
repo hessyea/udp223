4cc5b785b0c73c542f366a549640b4796f9a9e6d
hessyea <hasya101@gmail.com>
2017-12-06 22:04:45 +0100

    herokuapp.com/

--- bin/app2.js
+++ bin/app2.js
@@ -1,12 +1,12 @@
 
 var PORT = 80;
-var HOST = 'https://prim44.herokuapp.com';
+var HOST = 'https://udp22.herokuapp.com/';
 
 var dgram = require('dgram');
 var server = dgram.createSocket('udp4');
 
 server.on('listening', function () {
-    var address = server.address();
+    var address = server.address()+"/101";
     console.log('UDP Server listening on ' + address.address + ":" + address.port);
 });
 

0d4ce25fbe3ae51a7fc0816f6de7b888449bf140
hessyea <hasya101@gmail.com>
2017-12-06 22:09:19 +0100

    l8ter

--- bin/app2.js
+++ bin/app2.js
@@ -1,10 +1,10 @@
 
 var PORT = 80;
-var HOST = server.address();
+
 
 var dgram = require('dgram');
 var server = dgram.createSocket('udp4');
-
+var HOST = server.address();
 server.on('listening', function () {
     var address = server.address()+"/101";
     console.log('UDP Server listening on ' + address.address + ":" + address.port);

66a6e7d74aa97d450684044a0da00000e40cda17
hessyea <hasya101@gmail.com>
2017-12-06 22:15:49 +0100

    var os = require("os");
    var hostname = os.hostname();

--- bin/app2.js
+++ bin/app2.js
@@ -1,12 +1,12 @@
 
 var PORT = 80;
-
+var os = require("os");
+var HOST = os.hostname();
 
 var dgram = require('dgram');
 var server = dgram.createSocket('udp4');
-var HOST = server.address();
+
 server.on('listening', function () {
-    var address = server.address()+"/101";
     console.log('UDP Server listening on ' + address.address + ":" + address.port);
 });
 

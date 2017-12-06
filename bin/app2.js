698fdda4651a8f618d6427faf03c9bb79ee41ffb
hessyea <hasya101@gmail.com>
2017-12-06 22:33:59 +0100

    pushio

--- bin/app2.js
+++ bin/app2.js
@@ -1,18 +1,3 @@
-
-var PORT = 80;
-var os = require("os");
-var HOST = os.hostname();
-
-var dgram = require('dgram');
-var server = dgram.createSocket('udp4');
-
-server.on('listening', function () {
-    console.log('UDP Server listening on ' + address.address + ":" + address.port);
-});
-
-server.on('message', function (message, remote) {
-    console.log(remote.address + ':' + remote.port +' - ' + message);
-
-});
-
-server.bind(PORT, HOST);
+var io = require('socket.io')();
+io.on('connection', function(client){});
+io.listen(3000);
